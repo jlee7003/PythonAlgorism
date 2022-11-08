81b678666144319ec5efbc37ff41d5980307da27
ehlee3275 <ehlee3275@gmail.com>
2022-11-08 19:19:55 +0900

    20221108

--- 강의자료/알고리즘/구간합.py
+++ 강의자료/알고리즘/구간합.py
@@ -1,10 +1,19 @@
-A = [15, 13, 10, 7, 3, 12]
-S = [15,28,38,45,48,60]
+# #구간 합 만들어 보기
+# A = [15, 13, 10, 7, 3, 12]
+# prefix_sum = [0]
+# temp = 0
+# for i in A:
+#   temp = temp + i
+#   prefix_sum.append(temp)
+
+# print(prefix_sum)
+# print(prefix_sum[6]-prefix_sum[4])
 # for i in range(1,len(A)):
 # S[i] = S[i-1] + A[i]
-H = S[4]-S[2]  
-print(H)
-
+# H = S[4]-S[2]  
+# print(H)
+import sys
+input = sys.stdin.readline
 lineN, ansN =map(int,input().split(' ')) 
 lis = list(map(int,input().split(' ')))
 prefix_sum = [0]
@@ -17,4 +26,4 @@ for i in lis:
 for i in range(ansN):
   s, e = map(int,input().split(' ')) 
   print(prefix_sum[e]-prefix_sum[s-1])
-print(prefix_sum)
+# print(prefix_sum)
